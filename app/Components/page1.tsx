e916f1810e65cd54ea8dfa18e1b3672ecdc3a8aa
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 22:33:48 +0530

    Update page1.tsx

--- app/Components/page1.tsx
+++ app/Components/page1.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useState } from "react";
-
+import React, { useState, useEffect } from 'react';
 
 export default function Page1() {
+  
+  const [file, uploadFile]: any = useState();
 
-  const [msg, updateMsg] = useState("")
   const submit = async()=>{
     const formData = new FormData();
     formData.append("file", file);
@@ -22,10 +22,21 @@ export default function Page1() {
   }
   return (
     <>
+      <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600" onDragOver={(e) => e.preventDefault()} onDrop={(e) => {
+                    e.preventDefault();
+                    const data: any = e.dataTransfer.files;
+
+                    uploadFile(data[0]);
+
+                    updateFname(data[0].name);
+                    setUploadBool(true);
+
+                }}>
+        </label>
       <button onClick={submit}> Next page
       </button>
 
-      {msg}
+
     </>
   );
 }
