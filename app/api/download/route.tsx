54cee1df88d03d30fbdfe10fd843544af3039a08
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 16:02:49 +0530

    Update route.tsx

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -5,17 +5,20 @@ import { NextRequest, NextResponse } from 'next/server';
 export const fetchCache = "force-no-store";
 
 export async function GET(req:any){
-  const url = new URL(req.url)
-  const filename = url.searchParams.get("file") ;
-  const file = readFileSync(`./app/api/assets/${filename}`);
-  const headers = new Headers();
-  headers.append('Content-Disposition', `attachment; filename="${filename}"`);
-  headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-  headers.append('Content-Length', `${file.length}`)
-  headers.append('Content-Transfer-Encoding', 'binary')
-
-  return new Response(file, {
-    headers,
-  });
+  try{
+    const url = new URL(req.url)
+    const filename = url.searchParams.get("file") ;
+    const file = readFileSync(`./app/api/assets/${filename}`);
+    const headers = new Headers();
+    headers.append('Content-Disposition', `attachment; filename="${filename}"`);
+    headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    headers.append('Content-Length', `${file.length}`)
+    headers.append('Content-Transfer-Encoding', 'binary')
   
+    return new Response(file, {
+      headers,
+    });
+  } catch(e){
+    return NextResponse.json({message:"Failed"});
+  }
 }
