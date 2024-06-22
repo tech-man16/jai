919f41efd6fea1e5c8e3c59d471936dfd1e0d6de
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-22 15:39:51 +0530

    Update route.tsx

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -2,7 +2,7 @@ import { readFileSync, readdirSync } from 'fs';
 
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req:NextRequest) {
+export async function GET(req:NextRequest=null) {
   const url = new URL(req.url)
   const filename = url.searchParams.get("file") ;
   const file = readFileSync(`./app/api/assets/${filename}`);
