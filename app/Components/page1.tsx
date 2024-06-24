c88439d3cd53cb01383d24419c5a063b9f3754b5
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-24 15:51:57 +0530

    Update page1.tsx

--- app/Components/page1.tsx
+++ app/Components/page1.tsx
@@ -48,8 +48,9 @@ export default function Page1() {
     return (
         <>
             <div className="flex items-center justify-center w-96">
-                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600" 
-                    /*onDragOver={(e) => e.preventDefault()} onDrop={(e) => {
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    
+                    {/*onDragOver={(e) => e.preventDefault()} onDrop={(e) => {
                     e.preventDefault();
                     const data: any = e.dataTransfer.files;
 
@@ -58,7 +59,8 @@ export default function Page1() {
                     updateFname(data[0].name);
                     setUploadBool(true);
 
-                }}*/>
+                }}*/}
+                    
                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
 
                         <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
@@ -68,13 +70,14 @@ export default function Page1() {
                         <p className="text-xl text-gray-500 dark:text-gray-400">word doc or docx </p>
 
                     </div>
-                    <input id="dropzone-file" type="file" className="hidden" /*onChange={(e) => {
+                    <input id="dropzone-file" type="file" className="hidden" /> 
+                    {/*onChange={(e) => {
                         e.preventDefault();
                         const data: any = e.target.files;
                         uploadFile(data[0]);
                         updateFname(data[0].name);
                         setUploadBool(true);
-                    }}*/ />
+                    }}*/ }
                 </label>
 
 
