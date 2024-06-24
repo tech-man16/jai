d26c3e3112655fd321debd4f357207b77bdd431c
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-24 20:06:17 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -10,7 +10,7 @@ export const POST = async (req: NextRequest, res: any) => {
     
         const file = formData.get("file");
         return NextResponse.json({ data0: file  }, { status: 200 });
-        
+        /*
         if (!file) {
             // If no file is received, return a JSON response with an error and a 400 status code
             return NextResponse.json({ error: "No files received." }, { status: 400 });
@@ -28,7 +28,7 @@ export const POST = async (req: NextRequest, res: any) => {
             const url = new URL(req.url);
             return NextResponse.json({ message: 'Uploaded Failed', status: 500, "error":e , location:  path.join(`${url}`, '/app/api/assets/', file.name)}, { status: 500 })
         }
-        
+        */
     } catch(e){
         return NextResponse.json({ message: 'Loading...', status: 505 ,error: e}, { status: 505 });
     }
