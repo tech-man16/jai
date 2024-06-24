8764da87335fab111b2fa8c7cda75865e2d27d11
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-24 19:12:38 +0530

    Update route.tsx

--- app/api/example/route.tsx
+++ app/api/example/route.tsx
@@ -3,6 +3,9 @@ import { readFileSync, readdirSync } from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req:NextRequest) {
-  const url = new URL(req.url) ;
-  return NextResponse.json({urls:url},{status:200}) ; 
+  try {
+    const url = new URL(req.url) ;
+    return NextResponse.json({urls:url},{status:200}) ; 
+  } catch (e) {
+    return NextResponse.json({message: 500});
 }
