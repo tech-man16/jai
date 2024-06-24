7dfc91133dc482cdb209f6009fe7120d6a626844
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-06-24 20:02:09 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -4,9 +4,9 @@ import { writeFile,readdir } from "fs/promises";
 
 export const POST = async (req: NextRequest, res: any) => {
     try {
-        //const formData = await req.formData();
-        const data = await req.json();
-        return NextResponse.json({ data0: data  }, { status: 200 });
+        const formData = await req.formData();
+        //const data = await req.json();
+        return NextResponse.json({ data0: formData  }, { status: 200 });
         /*
         const file: File | null = data.get('file') as unknown as File;
         //const file = formData.get("file");
