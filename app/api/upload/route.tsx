b29625de7627403cbb2c468c0a43f729f02fbb95
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-07-09 21:54:44 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -26,12 +26,10 @@ export const POST = async (req: any, res: any) => {
             //await fs.writeFileSync(path.join('/tmp',file.name),buffer);
             //const file0 = fs.readdirSync('/tmp') ;
 
-            const file = readdirSync(path.join(process.cwd(),'/public/assets'));
-              try { return NextResponse.json({ message: file }); }
-              catch(e) { return NextResponse.json({ message: ["Loading..."] }) } 
-            //await fs.writeFileSync(path.join('/public/assets/',file.name),buffer);
-            //const file0 = fs.readdirSync(path.join('/public/assets')) ;
-            //return NextResponse.json({ message: 'Uploaded Successfully', status: 200, dirList: file0 }, { status: 200 }) ;
+            //const file = readdirSync(path.join(process.cwd(),'/public/assets'));
+            await fs.writeFileSync(path.join(process.cwd(),'/public/assets',file.name),buffer);
+            const file0 = readdirSync(path.join(process.cwd(),'/public/assets')) ;
+            return NextResponse.json({ message: 'Uploaded Successfully', status: 200, dirList: file0 }, { status: 200 }) ;
             
         }
         catch (e) {
