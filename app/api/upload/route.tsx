f7e37abff2a95ec269933ac0702b9e3ae2d06f40
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-07-10 09:37:04 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -14,7 +14,7 @@ export const POST = async (req: any, res: any) => {
             return NextResponse.json({ error: "No files received." }, { status: 400 });
         }
         const buffer = Buffer.from(await file.arrayBuffer());
-    
+        const PATH = path.resolve(path.join(process.cwd(),'/public/assets)) ;
         try {
             /*
             await fs.writeFile(path.join(process.cwd(),'/app/api/assets/', file.name), buffer,(err)=>{
@@ -27,17 +27,19 @@ export const POST = async (req: any, res: any) => {
             //const file0 = fs.readdirSync('/tmp') ;
 
             //const file = readdirSync(path.join(process.cwd(),'/public/assets'));
-            await writeFileSync(path.join(process.cwd(),'/public/assets',file.name),buffer);
-            const file0 = readdirSync(path.join(process.cwd(),'/public/assets')) ;
+            
+            //await writeFileSync(path.join(process.cwd(),'/public/assets',file.name),buffer);
+            await writeFileSync(PATH,buffer);
+            const file0 = readdirSync(PATH) ;
             return NextResponse.json({ message: 'Uploaded Successfully', status: 200, dirList: file0 }, { status: 200 }) ;
             
         }
         catch (e) {
-            console.log(process.cwd(), '/assets/', file.name);
-            console.log('\n\n\n', e)
+            //console.log(process.cwd(), '/assets/', file.name);
+           // console.log('\n\n\n', e)
             
-            const file0 = readdirSync(path.join(process.cwd(),'/public/assets')) ;
-            
+           // const file0 = readdirSync(path.join(process.cwd(),'/public/assets')) ;
+            const file0 = readdirSync(PATH) ;
             return NextResponse.json({ message: 'Uploaded Failed', status: 500, "error":e , 
                                       location:  path.join('/assets/',file.name),
                                       fileList : file0,
