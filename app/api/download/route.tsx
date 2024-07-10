4de6754d6231ab7bdb655b409cd5e11746f09ab3
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-07-10 13:36:14 +0530

    Update route.tsx

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -9,7 +9,7 @@ export async function GET(req:NextRequest) {
     const filename = url.searchParams.get("file") ;
     //const file = readFileSync(`./app/api/assets/${filename}`);
     //const PATH = path.join(process.cwd(),`public/assets/${filename}`) ;
-    const PATH = path.join(`/assets/${filename}`) ;
+    const PATH = path.join(`/tmp/${filename}`) ;
     const file = readFileSync(PATH);
     const headers = new Headers();
     headers.append('Content-Disposition', `attachment; filename="${filename}"`);
@@ -30,7 +30,7 @@ export async function GET(req:NextRequest) {
 export async function POST(req: any, res: any) {
   //const file = readdirSync(process.cwd() + '/app/api/assets')
   //const file = readdirSync(path.join(process.cwd(),'/public/assets'));
-  const file = readdirSync(path.join('/assets'));
+  const file = readdirSync('/tmp');
   try { return NextResponse.json({ message: file }); }
   catch(e) { return NextResponse.json({ message: ["Loading..."] }) } 
 }
