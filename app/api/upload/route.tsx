e83a1f0e6f15b934bca84ec00d7620b11ef5ef00
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-07-10 10:02:18 +0530

    Update route.tsx

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -29,8 +29,8 @@ export const POST = async (req: any, res: any) => {
             //const file = readdirSync(path.join(process.cwd(),'/public/assets'));
             
             //await writeFileSync(path.join(process.cwd(),'/public/assets',file.name),buffer);
-            await writeFileSync(PATH,buffer);
-            const file0 = readdirSync(PATH) ;
+            await writeFileSync(path.join(process.cwd(),'/app/api/assets/', file.name),buffer);
+            const file0 = readdirSync(path.join(process.cwd(),'/app/api/assets/')) ;
             return NextResponse.json({ message: 'Uploaded Successfully', status: 200, dirList: file0 }, { status: 200 }) ;
             
         }
@@ -39,9 +39,9 @@ export const POST = async (req: any, res: any) => {
            // console.log('\n\n\n', e)
             
            // const file0 = readdirSync(path.join(process.cwd(),'/public/assets')) ;
-            const file0 = readdirSync(PATH) ;
+            const file0 = readdirSync(path.join(process.cwd(),'/app/api/assets/')) ;
             return NextResponse.json({ message: 'Uploaded Failed', status: 500, "error":e , 
-                                      location:  path.join('/assets/',file.name),
+                                      location:  path.join(process.cwd(),'/app/api/assets/'), //path.join('/assets/',file.name),
                                       fileList : file0,
                                      }, { status: 500 })
         }
